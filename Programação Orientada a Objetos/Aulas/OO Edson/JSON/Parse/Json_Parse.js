9c9f54cc96ce25537497fb2ed69173651fbb081f
Bruno Assis <62751002+BalluBR@users.noreply.github.com>
2023-01-16 22:12:19 -0300

    Update Json_Parse.js

--- Programação Orientada a Objetos/Aulas/OO Edson/JSON/Parse/Json_Parse.js
+++ Programação Orientada a Objetos/Aulas/OO Edson/JSON/Parse/Json_Parse.js
@@ -1,4 +1,4 @@
- // É possível trasnformar um texto em um objeto através da propriedade parse do JSON
+ // É possível transformar um texto em um objeto através da propriedade parse do JSON
 
  let user = '{"name":"joão","age":"20","isAdmin":"false","friends":["Maria",1,2,3]}' //usar aspas simples por fora
 
